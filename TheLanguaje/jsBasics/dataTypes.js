81eeb493041cc1fd194e6e68ce39db64878385cb
yan pizarro <pizarroestevez2@gmail.com>
2024-12-14 13:56:50 -0300

    variables and data Types of js Basics

---
+++ TheLanguaje/jsBasics/dataTypes.js
@@ -0,0 +1,34 @@
+//numbers
+let age = 36;
+let salary = 1000.666;
+console.log(age + " " + salary);
+
+//specials numbers
+let infinity = Infinity;
+let nan ='pepe' / 2;
+console.log(infinity + ' ' +nan)
+
+//bigInt
+let bigInt = 1234567890123456789012345678901234567890n;
+console.log(typeof bigInt)
+
+//string
+let single = 'single';
+let double = "double";
+let backstic = `backsticks ${33}`
+console.log(single + ' ' + double + ' ' + backstic);
+
+//boolean
+let isDoctor = true;
+let isEnginier = false;
+let mayor = 3 > 5;
+console.log( isDoctor + ' ' + isEnginier + ' ' + mayor)
+
+//null
+let userName = null;
+console.log(userName);
+
+//undefined
+let lastName = undefined;
+let lastAge;
+console.log(lastName + ' ' + lastAge);
---
+++ TheLanguaje/jsBasics/variables.js
@@ -0,0 +1,28 @@
+'use strict'
+
+let message, name = '', lastName = 'pizarro' ;
+message = 'hello';
+name = 'yan';
+let age = 36;
+//country is undefined because of 'use strict'
+console.log(message + ' ' + name + ' ' + ' ' + lastName + ' ' + age + ' ' + country);
+
+var country = 'cuba';
+console.log(country);
+
+let numbersFirst = 1;
+let numbersSecond = numbersFirst;
+console.log(numbersFirst + numbersSecond);
+
+const myBirthday = '20/10/1988';
+//myBirthday = 'xxxx';
+console.log(myBirthday);
+
+const COLOR_RED = '#F00';
+let color = COLOR_RED;
+console.log(color);
+
+
+
+
+
