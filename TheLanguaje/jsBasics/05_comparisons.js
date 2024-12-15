37be4ea2b14d99599867ef0049ec79d0e4c5c2d9
yan pizarro <pizarroestevez2@gmail.com>
2024-12-15 14:02:22 -0300

    05 comparisons js Baiscs

---
+++ TheLanguaje/jsBasics/05_comparisons.js
@@ -0,0 +1,44 @@
+console.log(2 > 1);
+console.log(3 < 1);
+console.log(2 == 4);
+console.log(3 != 6);
+
+let result = 3 < 7;
+console.log(result);
+console.log('pepe' > 'pepepe');
+console.log('pepe'.length > 'pep'.length);
+
+console.log('2' > 1);
+console.log('05' < 9);
+console.log('2r' > 1);
+
+console.log(true > 3);
+console.log(false < 4);
+
+
+console.log( 0 === false);
+console.log( 0 == false);
+console.log( '' == false);
+console.log( '' == 0);
+
+console.log(null == undefined);
+console.log(null === undefined);
+
+console.log( null > 0 );
+console.log( null == 0 );
+console.log( null >= 0 );
+
+console.log( undefined > 0 );
+console.log( undefined < 0 );
+console.log( undefined == 0 );
+console.log( undefined == null );
+
+console.log( null > 0)
+
+console.log( 5 > 4);
+console.log( 'apple' > 'pineapple');
+console.log( '2' > '12');
+console.log( undefined == null);
+console.log(undefined === null)
+console.log(null == "\n0\n");
+console.log(null === +'\n0\n');
